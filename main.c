fef4a61fc5d747ef7c8bd3d56ab5a4a8f382d0b3
Kyle Laker <kyle@laker.email>
2018-07-14 15:25:55 -0400

    Add find calls to main

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "list.h"
@@ -5,24 +6,35 @@
 int main(void) {
 
     list_t *list = list_create();
-    printf("Get 0?: %d\n", list_get(*list, 0));
+    printf("Get idx 0?: %d\n", list_get(*list, 0));
+
+    printf("\n== Add to list ==\n");
     for (int i = 1; i <= 10; i++) {
         list_append(list, i);
     }
 
+    printf("\n== Get each item==\n");
     for (int i = 0; i < 10; i++) {
         int32_t val = list_get(*list, i);
         printf("%d: %d\n", i, val);
     }
 
+    printf("\n== Find each item==\n");
+    for (int i = 1; i <= 10; i++) {
+        size_t loc = list_find(*list, i);
+        printf("%ld @ %d\n", loc, i);
+    }
+
+    printf("\n== Delete each item==\n");
     for (int i = 9; i >= 0; i--) {
         int32_t val = list_delete(list, i);
         printf("%d: %d\n", i, val);
     }
 
-    printf("empty?: %d\n", list_is_empty(*list));
+    printf("\n");
+    printf("empty?: %s\n", list_is_empty(*list) ? "Yes" : "No");
 
-    printf("Get 0?: %d\n", list_get(*list, 0));
+    printf("Get idx 0?: %d\n", list_get(*list, 0));
 
     printf("Destroying list.\n");
 
